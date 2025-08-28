e95c2bda5e5d99a15f4c8b858897ef8d112b9619
ErickSarmiento010807 <L25550485@chihuahua2.tecnm.mx>
2025-08-27 21:31:42 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Hogar
+ */
+public class EVA1_5_VARIABLES {
+
+
+    public static void main(String[] args) {
+        //DECLARACION DE VARIABLES
+        //Crearlas las variables para que se puedan usar
+        //TIPO DE DATO  IDENFITICADOR
+        String nombre;
+        byte edad;
+        //INICIALIZACIÓN
+        //USAMO EL OPERADOR =  ---> OPERADOR DE ASIGNACIÓN
+        //PON "Erick Sarmiento" dentro de la variable de nombre
+        nombre = "Erick Sarmiento";
+        edad = 18;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
